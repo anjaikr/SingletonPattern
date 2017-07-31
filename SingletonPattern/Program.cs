67ef1c0fec3437567995e89d89d5d791648737d0
anjaikr <anjaikr@gmail.com>
2017-07-31 17:04:11 +0530

    SingletonPattern

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -4,12 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SingletonPattern
 {
+
+    public class ReportingSingleton
+    {
+        private static ReportingSingleton instance = null;
+
+        public int numReport { get; set; }
+        private ReportingSingleton()
+        { }
+        public static ReportingSingleton GetInstance()
+        {
+            lock (typeof(ReportingSingleton))
+            {
+                if (instance == null)
+                {
+                    instance = new ReportingSingleton();
+                }
+                return instance;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            GetReportingMethodfromA();
+            GetReportingMethodfromB();
+            GetReportingMethodfromC();
+            Console.ReadLine();
+        }
+
+        public static void GetReportingMethodfromA()
+        {
+            var singletonInstancefromA = ReportingSingleton.GetInstance();
+            singletonInstancefromA.numReport++;
+            Console.WriteLine(singletonInstancefromA.numReport.ToString());
+
+        }
+        public static void GetReportingMethodfromB()
+        {
+            var singletonInstancefromB = ReportingSingleton.GetInstance();
+            singletonInstancefromB.numReport++;
+            Console.WriteLine(singletonInstancefromB.numReport.ToString());
+        }
+
+        public static void GetReportingMethodfromC()
+        {
+            var singletonInstancefromC = ReportingSingleton.GetInstance();
+            singletonInstancefromC.numReport++;
+            Console.WriteLine(singletonInstancefromC.numReport.ToString());
         }
     }
 }
